d201a3aa8799e3266d8d6a35c1d0ff92752a9a56
larsac07 <larsac07@gmail.com>
2016-05-14 23:36:42 +0200

    Use XMLComparator in Main.

--- src/no/uib/lca092/cisqAnalyzerComparator/Main.java
+++ src/no/uib/lca092/cisqAnalyzerComparator/Main.java
@@ -5,48 +5,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
+import no.uib.lca092.cisqAnalyzerComparator.compare.XMLComparator;
 import no.uib.lca092.cisqAnalyzerComparator.io.IOUtils;
 import no.uib.lca092.cisqAnalyzerComparator.io.XMLReader;
 
 public abstract class Main {
 
+	private final static String LOG_FILES = "Found the following files:";
+	private final static String LOG_PARSING = "Parsing to XML:";
+
 	public static void main(String[] args) {
 		File folder = new File("/home/lars/Development/java_workspace/SQuIDS/compare");
+
 		String fileEnding = "result.xml";
+
 		List<String> excludes = new ArrayList<>();
 		excludes.add(".*result\\.xml");
+
 		List<String> includes = new ArrayList<>();
 		includes.add(".*checkstyle.*");
 		includes.add(".*Checkstyle.*");
-		includes.add(".*RSSOwl.*");
+		includes.add(".*RSSOwl 1\\.2\\.4.*");
+		includes.add(".*RSSOwl Source.*");
 		includes.add(".*rssowl.*");
-		includes.add(".*SQuIDS/TV-Browser.*");
-		includes.add(".*TV-Browser 2\\.2\\.5.*");
 		includes.add(".*JabRef.*");
 		includes.add(".*Log4j.*");
+		includes.add(".*log4j.*");
+
 		List<File> files = IOUtils.getFiles(folder, fileEnding, excludes, includes);
-		System.out.println(files.size() + " files");
+		System.out.println(LOG_FILES);
 		for (File file : files) {
 			System.out.println(file.getAbsolutePath());
 		}
+
+		List<Document> docs = new ArrayList<>();
 		XMLReader reader = new XMLReader();
+		System.out.println(LOG_PARSING);
 		for (File xmlFile : files) {
 			System.out.println(xmlFile.getAbsolutePath());
 			Document doc = reader.getXMLDoc(xmlFile);
-			Elements items = doc.getElementsByTag("item");
-			for (Element item : items) {
-				System.out.println("-----------------------------------");
-				System.out.println(item.getElementsByTag("metric").get(0));
-				System.out.println(item.getElementsByTag("file").get(0));
-				System.out.println(item.getElementsByTag("line").get(0));
-				System.out.println(item.getElementsByTag("detail").get(0));
-				System.out.println("-----------------------------------");
-				System.out.println();
-			}
+			docs.add(doc);
 		}
+
+		XMLComparator comp = new XMLComparator();
+		comp.compare(docs);
 	}
 
 }
